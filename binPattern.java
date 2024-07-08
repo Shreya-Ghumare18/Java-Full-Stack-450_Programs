fbdf27dd1d5cfd7685e463de68e2c04fe5832a30
Shreya-Ghumare18 <159309725+Shreya-Ghumare18@users.noreply.github.com>
2024-07-08 08:49:59 +0530

    Add files via upload

---
+++ binPattern.java
@@ -0,0 +1,13 @@
+public class binPattern {
+    public static void main(String[] args) {
+        int i,j;
+        for (i=0;i<3;i++) {
+            for(j=0;j<2;j++){
+            System.out.println(i+" "+j);
+            }
+        
+        }
+    }
+
+    }
+
